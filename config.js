5977da5709c5ea3d52dc60d0ce0131427f3bca9e
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2022-05-05 20:40:26 +0300

    Configuration changes

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ var SlackBot = require('slackbots');
 
 module.exports = {
   REACTION_NAME: process.env.REACTION_NAME || 'balloon',
-  SLACK_NAME: process.env.SLACK_NAME || 'nomedu',
+  SLACK_NAME: process.env.SLACK_NAME || 'artjoman',
   SLACK_TOKEN: process.env.SLACK_TOKEN.substr(),
-  DIGEST_CHANNEL: process.env.DIGEST_CHANNEL || 'balloon-bot',
-  BOT_NAME: process.env.BOT_NAME || 'Balloon Bot',
-  ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'adrienj'
+  DIGEST_CHANNEL: process.env.DIGEST_CHANNEL || 'techies-of-baltics-bot',
+  BOT_NAME: process.env.BOT_NAME || 'Techies of Baltics Bot',
+  ADMIN_USERNAME: process.env.ADMIN_USERNAME || 'artjoman'
 };
 
 // create a bot
