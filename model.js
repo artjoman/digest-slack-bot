c340913ec7e03e45081cffe50ac3aaecdef9dbbd
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2022-05-05 22:08:24 +0300

    Fix Mongoose 3

--- model.js
+++ model.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 const options = {
-  useMongoClient: true
+  useNewUrlParser: true
 };
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/balloon-bot', options);
