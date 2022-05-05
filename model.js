1296de80afa1d34948f4349f0cbd12354af60853
shegoestech202122 <artjoms.v.petrovs@gmail.com>
2022-05-05 22:06:41 +0300

    Fix Mongoose 2

--- model.js
+++ model.js
@@ -1,13 +1,7 @@
 var mongoose = require('mongoose');
 
 const options = {
-  useMongoClient: true,
-  autoIndex: false, // Don't build indexes
-  reconnectTries: 20,
-  reconnectInterval: 5000, // Reconnect every 500ms
-  poolSize: 10, // Maintain up to 10 socket connections
-  // If not connected, return errors immediately rather than waiting for reconnect
-  bufferMaxEntries: 0
+  useMongoClient: true
 };
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/balloon-bot', options);
